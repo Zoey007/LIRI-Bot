5166ee263f25e605c172faa585494960138147b6
Zoey007 <guiyuinusa@gmail.com>
2017-08-23 17:07:09 -0700

    Used Twitter and Spotify APIs and NPM packages. Used Node file append for storing output info. Still need to figure out why tweets are out of order in the output file, and modify the spotify search input so that the search query grab the entire song name.

---
+++ keys.js
@@ -0,0 +1,16 @@
+//Author: Guiyu (Zoey) Zhao
+//Created 8/23/2017
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '7aKl8sgXv2DvqCDI7FlK5KQYD',
+  consumer_secret: 'RAqSXNA90rDlyLwczx9FhQj6SMsSvwan50PzLD1uXzipmQIGKS',
+  access_token_key: '900483166506434560-K3cxT4EFhAw6jOonkKgNGeiJ0EwiGqq',
+  access_token_secret: 'QMowQzJ6r4eRRBzADa8828tR17NGaxakrxs0Rtstr15Zb',
+}
+
+exports.SpotifyKeys = {
+    client_id: '3f680a130a7e4bc4aa6ef434e02da8fb',
+    client_secret: 'be08247b3cc54a678318309be396be0d',
+  }
